0b329784faa1b6e9fb6796505521599581012723
BlueJadees <128740907+BlueJadees@users.noreply.github.com>
2025-04-27 22:19:57 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,17 +28,16 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
+        {todos.map(todo => <li
           onClick={() => deleteTodo(todo.id)}
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
       </div>
     </main>
   );
